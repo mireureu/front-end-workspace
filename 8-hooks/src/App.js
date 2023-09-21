b958f72c1de0115a2309d1a8799bc7b3b26697fd
mireureu <j96410@naver.com>
2023-09-21 09:10:52 +0900

    20230921

--- 8-hooks/src/App.js
+++ 8-hooks/src/App.js
@@ -1,26 +1,7 @@
-import { useState } from "react";
-const Button = ({text, click}) =>{
-  return <button onClick={click}>{text}</button>
-}
-
+import Converter from "./components/Converter";
 const App = () =>{
-  const [counter, setCounter] = useState(0);
-  const setCount = () => {
-    setCounter(counter + 1);
+  return <Converter/>;
 };
-const minusCount = () =>{
-  setCounter(counter -1);
-}
-  return ( 
-    <div>
-        <h1>Total clicks: {counter}</h1>
-        <Button text="+1" click={setCount}/>
-        <Button text="-1" click={minusCount}/>
-        {/*<button onClick={setCount}>+1</button>
-        <button onClick={minusCount}>-1</button>*/}
-    </div>
-  );
-} 
 
 export default App;
 
